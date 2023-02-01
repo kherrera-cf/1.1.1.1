6c99069f52a190d599188b2ab4ae426b9037185e
Samwel Charles <scharles@cloudflare.com>
2023-02-01 17:08:28 +0300

    feat(1.1.1.1): MRK-16077 Add language picker

--- source/pages/family.ts
+++ source/pages/family.ts
@@ -1,3 +1,33 @@
 import '../styles/index.css'
-
+import '../styles/nav.styl'
 import './init.js'
+
+function ref(ref: string): HTMLElement {
+    return <HTMLElement>document.querySelector(`[data-ref="${ref}"]`)!;
+}
+
+function init() {
+
+    const $el = {
+        languageSelector: ref("languageSelector")
+    };
+
+    document.addEventListener("click", event => {
+
+        if (
+            event.target === $el.languageSelector ||
+            $el.languageSelector.contains(<Node>event.target)
+        ) {
+            $el.languageSelector.classList.toggle("dropdown-open");
+        } else {
+            $el.languageSelector.classList.toggle("dropdown-open", false);
+        }
+    });
+}
+
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", init);
+} else {
+    init();
+}
+
